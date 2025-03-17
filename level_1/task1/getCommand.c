c6e5b7e6252dfda0d7fd5c499d966197bc80fb06
LiuZhaoyan <1626259015@qq.com>
2025-03-17 20:09:31 +0800

    level_1 task1 maybe completed

---
+++ level_1/task1/getCommand.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print_help(){
+	printf("Usage:\n");
+	printf("Options:\n");
+	printf("--help    Display help message\n");
+}
+
+int main(int argc,char *argv[]){
+	//如果没有提供参数
+	if(argc==1){
+	printf("Missing parameters\n");
+	return 1;
+	}
+	for(int i=1;i<argc;i++){
+	if(strcmp(argv[i],"--help")==0){
+	print_help();
+	}else{
+	printf("Error: Unknown option '%s'\n",argv[i]);
+	return 1;
+	}
+
+	}
+	return 0;
+}
